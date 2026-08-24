56e5d933e73afc66e4e31c4ce407a6c410233ca0
Hari Praghash <hari.praghash@pollinate.co.uk>
2026-08-24 14:40:07 +0100

    feat(infra): deploy Strimzi Kafka resources (#34)

--- infra/disposable/variables.tf
+++ infra/disposable/variables.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 5.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.2.0"
+    }
   }
 
   backend "azurerm" {}
